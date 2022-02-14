8d53d07aa73a5c52e717cda1da34596ea8cd53f5
hall9zeha <vmwaretars@gmail.com>
2022-02-14 09:34:08 -0500

    agregado título a histoial compras y seguimiento

--- NiloPartner/app/src/main/java/com/barryzea/niloclient/track/TrackFragment.kt
+++ NiloPartner/app/src/main/java/com/barryzea/niloclient/track/TrackFragment.kt
@@ -2,9 +2,13 @@ package com.barryzea.niloclient.track
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.barryzea.niloclient.R
 import com.barryzea.niloclient.databinding.FragmentTrackBinding
 import com.barryzea.niloclient.interfaces.OrderAux
 import com.barryzea.niloclient.pojo.Order
@@ -28,6 +32,22 @@ class TrackFragment:Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getOrder()
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        (activity as? AppCompatActivity)?.let{
+            it.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            it.supportActionBar?.title=getString(R.string.track_title)
+            setHasOptionsMenu(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==android.R.id.home){
+            activity?.onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun getOrder() {
@@ -45,5 +65,14 @@ class TrackFragment:Fragment() {
             it.cbSent.isChecked=order.status>2
             it.cbDelivered.isChecked=order.status>4
         }
+    }
+
+    override fun onDestroy() {
+        (activity as? AppCompatActivity)?.let{
+            it.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            it.supportActionBar?.title=getString(R.string.order_title)
+            setHasOptionsMenu(false)
+        }
+        super.onDestroy()
     }
 }
