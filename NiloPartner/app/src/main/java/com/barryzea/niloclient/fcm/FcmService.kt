d2b0919afe81df5087c9fe8a37cfc830115cf7d9
hall9zeha <vmwaretars@gmail.com>
2022-02-16 11:18:21 -0500

    Notificacion de cloud messaging en primer plano implementada

--- NiloPartner/app/src/main/java/com/barryzea/niloclient/fcm/FcmService.kt
+++ NiloPartner/app/src/main/java/com/barryzea/niloclient/fcm/FcmService.kt
@@ -1,9 +1,20 @@
 package com.barryzea.niloclient.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
+import com.barryzea.niloclient.MainActivity
+import com.barryzea.niloclient.R
 import com.barryzea.niloclient.commons.Constants
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class FcmService: FirebaseMessagingService() {
     override fun onNewToken(newToken: String) {
@@ -16,5 +27,40 @@ class FcmService: FirebaseMessagingService() {
             putString(Constants.PROPERTY_TOKEN, newToken)
                 .apply()
         }
+    }
+//manejando las notificaciones en primer plano
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        remoteMessage.notification?.let{
+            sendNotification(it)
+        }
+    }
+    private fun sendNotification(remoteMessage: RemoteMessage.Notification){
+        val intent=Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent= PendingIntent.getActivity(this, 0,intent,PendingIntent.FLAG_ONE_SHOT)
+        val channelId=getString(R.string.notification_channel_id)
+        val defaultSoundUri=RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder=NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_stat_name)
+            .setContentTitle(remoteMessage.title)
+            .setContentText(remoteMessage.body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        //creamos el canal de la notificación ya ques obligatoria a artir de android 8
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val channelName=getString(R.string.channel_name_nilo)
+            val description=getString(R.string.description_channel)
+            val importance =NotificationManager.IMPORTANCE_HIGH
+            val mChannel=NotificationChannel(channelId, channelName,importance)
+            mChannel.description=description
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+
     }
 }
